87058125a074efcc40e7589245a6eb524fbdab1f
HenBarper <Benjaminitonio@gmail.com>
2023-04-21 08:46:32 -0700

    3 quick lomuto test

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,28 +1,73 @@
 #include "sort.h"
 
 /**
- * quick_sort - sort array with qucik sort alg
- * @array: array to sort
- * @size: size of array
+ * quick_sort - function that sorts an array of integers
+ *              in ascending order using the Quick sort algorithm
+ * @array: array
+ * @size: array's size
  * Return: void
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t i = 0, j, pivot;
-	int temp;
+	if (array == NULL || size < 2)
+		return;
 
-	pivot = size;
+	quick_s(array, 0, size - 1, size);
+}
 
-	if (array[i] > array[pivot])
+/**
+ * partition - partition
+ * @array: array
+ * @lo: lower
+ * @hi: higher
+ * @size: array's size
+ * Return: i
+ */
+int partition(int *array, int lo, int hi, size_t size)
+{
+	int i = lo - 1, j = lo;
+	int pivot = array[hi], aux = 0;
+
+	for (; j < hi; j++)
 	{
-		temp = array[i];
-		array[i] = array[pivot];
-		array[pivot] = temp;
-		print_array(array, size);
-		i++;
+		if (array[j] < pivot)
+		{
+			i++;
+			if (array[i] != array[j])
+			{
+				aux = array[i];
+				array[i] = array[j];
+				array[j] = aux;
+				print_array(array, size);
+			}
+		}
 	}
-	else
+	if (array[i + 1] != array[hi])
 	{
-		pivot--;
+		aux = array[i + 1];
+		array[i + 1] = array[hi];
+		array[hi] = aux;
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * quick_s - quick sort
+ * @array: given array
+ * @lo: lower
+ * @hi:higher
+ * @size: array's size
+ * Return: void
+ */
+void quick_s(int *array, int lo, int hi, size_t size)
+{
+	int pivot;
+
+	if (lo < hi)
+	{
+		pivot = partition(array, lo, hi, size);
+		quick_s(array, lo, pivot - 1, size);
+		quick_s(array, pivot + 1, hi, size);
 	}
 }
